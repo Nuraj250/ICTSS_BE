a12340c031b37d60d0761f0a43539b89ce772729
Nuraj250 <nurajshaminda200@gmail.com>
2024-10-03 20:54:17 -0600

    init

---
+++ src/main/java/com/esoft/ICTSS/IctssApplication.java
@@ -0,0 +1,13 @@
+package com.esoft.ICTSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IctssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IctssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esoft/ICTSS/IctssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esoft.ICTSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IctssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
